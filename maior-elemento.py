b2bfc7aa383993ce9f7eefefb79afbebdf4153ab
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-25 13:50:53 -0300

    Create maior-elemento.py

---
+++ maior-elemento.py
@@ -0,0 +1,6 @@
+# Escreva a função maior_elemento que recebe como parâmetro uma lista com números inteiros e devolve um número inteiro correspondente ao maior valor presente na lista recebida.
+def maior_elemento(list):
+    list.sort()
+    return list[-1]
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
